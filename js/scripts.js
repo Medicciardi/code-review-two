bae8be933b6786128cde9823b79375e9c895a143
Andrew <Andreww716@gmail.com>
2016-09-25 14:15:00 -0700

    Add css and js directories, and index.html file. Adds radio button inputs and begins to lay out js functions to decide destination based on user input.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//Front-end logic
+$().ready(function(){
+  $("form#vacation").submit(function(){
+    event.preventDefault();
+    var choice1 = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "ice") {
+      result = 
+    }
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Back-end logic
+var swiss = function (choice1){
+
+}
