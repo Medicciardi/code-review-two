6d861919e86d93a7477b35d36cb90a929f32f2a0
Andrew <Andreww716@gmail.com>
2016-09-29 10:51:54 -0700

    Add pictures to img folder and write JavaScript that will take user input and assign them a destination based on an activity and budget inquiry. An image will show of the destination when the form is submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 //Front-end logic
 $().ready(function(){
-  $("form#vacation").submit(function(){
+  $("form.vacation").submit(function(event){
     event.preventDefault();
-    var choice1 = $("input:radio[name=operator]:checked").val();
-    var result;
-    if (operator === "ice") {
-      result = 
+    var choice = $("input:radio[name=operator]:checked").val();
+    if (choice === "ice") {
+      $("#swiss").show();
+    } else if (choice === "kayak") {
+      $("#patagonia").show();
+    } else if (choice === "scuba") {
+      $("#hawaii").show();
+    } else if (choice === "lounge") {
+      $("#fiji").show();
+    } else if (choice === "lounge" && "budget4") {
+      $("#hawaii").show();
+    } else if (choice === "scuba" && "budget1") {
+      $("#fiji").show();
+    } else if (choice === "lounge" && "budget3") {
+      $("#swiss").show();
     }
-  })
+
+  });
 });
 
 
@@ -22,12 +34,4 @@ $().ready(function(){
 
 
 
-
-
-
-
-
 //Back-end logic
-var swiss = function (choice1){
-
-}
