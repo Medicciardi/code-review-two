b421b9a865c8c0f191f368ddf2b0fd7c4c26674f
Andrew <Andreww716@gmail.com>
2016-09-30 13:59:30 -0700

    Sync code up with local repository

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,25 @@ $().ready(function(){
   $("form.vacation").submit(function(event){
     event.preventDefault();
     var choice = $("input:radio[name=operator]:checked").val();
-    if (choice === "ice") {
+    var choice2 = $("input:radio[name=operator2]:checked").val();
+    if (choice === "ice" && choice2 === "budget1") {
       $("#swiss").show();
     } else if (choice === "kayak") {
+      $(".destination").children().hide();
       $("#patagonia").show();
-    } else if (choice === "lounge") {
-      $("#fiji").show();
     } else if (choice === "scuba") {
+      $(".destination").children().hide();
+      $("#hawaii").show();
+    } else if (choice === "lounge") {
+      $(".destination").children().hide();
+      $("#fiji").show();
+    } else if (choice === "ice") {
+      $(".destination").children().hide();
+      $("#patagonia").show();
+    } else {
+      $(".destination").children().hide();
       $("#hawaii").show();
     }
-
-
   });
 });
 
